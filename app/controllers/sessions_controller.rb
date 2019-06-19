20a5df5fc3f1bb552ac8c0166d0b44d04a6ffc7d
2daysweb <sahnunhm@gmail.com>
2019-06-19 12:22:21 -0400

    Added login and logout paths, sessions controller updated

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+  	@user = User.find_by(username: params[:username])
+  	if @user 
+  		session[:user_id] = @user.id 
+  		redirect_to my_team_path 
+  	else 
+  		flash.notice = "No user found with that username"
+  		render :new 
+  	end 
+  end
+
+  def destroy 
+  	session.clear 
+  	redirect_to login_path 
+  end  
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'my_team/index'
+  get "/login", to: "sessions#new", as: "login" 
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy", as: "logout"
+  get 'my_team/index', as: "my_team"
   resources :players, only: [:index, :show]
   resources :my_team, only: [:index]
 

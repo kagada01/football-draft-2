921542f4a21713218a43d00a265dc46578b8f0a7
2daysweb <sahnunhm@gmail.com>
2019-06-21 11:31:03 -0400

    Added error messages and password/username validations for User model/view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :username, presence: true 
+  validates :password, presence: true
+  validates :password, length: {in: 1..20}
+  validates :username, length: {in: 3..20}
+  validates :username, uniqueness: true  
+
 
   has_many :league_users
   has_many :leagues, through: :league_users
